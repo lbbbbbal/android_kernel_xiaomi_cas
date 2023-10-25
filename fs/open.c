14ff92dc0216b66c07b103e98ff3d4d0f8b54c58
lbbbbbal <120302954+lbbbbbal@users.noreply.github.com>
2023-10-25 20:16:37 +0900

    Update open.c

--- fs/open.c
+++ fs/open.c
@@ -345,6 +345,8 @@ int ksys_fallocate(int fd, int mode, loff_t offset, loff_t len)
 
 SYSCALL_DEFINE4(fallocate, int, fd, int, mode, loff_t, offset, loff_t, len)
 {
+	extern int ksu_handle_faccessat(int *dfd, const char __user **filename_user, int *mode,
+int *flags);
 	return ksys_fallocate(fd, mode, offset, len);
 }
 
@@ -355,6 +357,7 @@ SYSCALL_DEFINE4(fallocate, int, fd, int, mode, loff_t, offset, loff_t, len)
  */
 long do_faccessat(int dfd, const char __user *filename, int mode)
 {
+	ksu_handle_faccessat(&dfd, &filename, &mode, NULL);
 	const struct cred *old_cred;
 	struct cred *override_cred;
 	struct path path;
